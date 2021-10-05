0d7c8da234239d907d2ab1b3a1e30ae4c0fed9d8
Erik O Gabrielsson <erik.o.gabrielsson@sectra.com>
2021-10-05 08:57:27 +0200

    Add simple CLI

--- README.md
+++ README.md
@@ -11,25 +11,35 @@ Please note that this is an early release and the API is not frozen yet. Functio
 Files with z-stacks or multiple focal paths are currently not supported.
 
 ## Basic usage
-***Import a ndpi-file into a WsiDicom object.***
+***Convert a wsi-file into DICOM using cli-interface***
+```bash
+wsidicomizer/cli.py -i 'path_to_wsi_file' -o 'path_to_output_folder'
+```
+### Arguments:
+-i, --input, path to input wsi file  
+-o, --output, path to output folder  
+-d, --dataset, optional path to json file defining base dataset  
+-t, --tile_size, tile size, required depending on input format  
+-l, --levels, optional levels to include  
+
+### Flags
+--no_label, do not include label(s)  
+--no_overview, do not include overview(s)  
+
+***Convert a wsi-file into DCIOM using python-interface***
 ```python
 from wsidicomizer import WsiDicomizer
-tile_size = (1024, 1024)
-wsi = WsiDicomizer.import_tiff(
-    path_to_ndpi_file,
+WsiDicomizer.convert(
+    path_to_wsi_filee,
+    path_to_export_folder,
     tile_size
 )
-region = wsi.read_region((1000, 1000), 6, (200, 200))
-wsi.close()
 ```
 
-***Convert a Ndpi-file into Dicom files. Use a (test) base dataset that will be common for all created Dicom instances.***
+***Import a wsi file as a WsiDicom object.***
 ```python
-base_dataset = WsiDataset.create_test_base_dataset()
-WsiDicomizer.convert(
-    path_to_ndpi_file,
-    path_to_export_folder,
-    base_dataset,
-    tile_size
-)
+from wsidicomizer import WsiDicomizer
+wsi = WsiDicomizer.import_tiff(path_to_wsi_file)
+region = wsi.read_region((1000, 1000), 6, (200, 200))
+wsi.close()
 ```
---
+++ wsidicomizer/cli.py
@@ -0,0 +1,68 @@
+import argparse
+from pathlib import Path
+import json
+from pydicom.dataset import Dataset
+from wsidicomizer.interface import WsiDicomizer
+from wsidicomizer.dataset import create_test_base_dataset
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert wsi to DICOM')
+
+    parser.add_argument(
+        '-i', '--input',
+        type=Path,
+        help='path to input wsi file'
+    )
+    parser.add_argument(
+        '-o', '--output',
+        type=Path,
+        help='path to output folder'
+    )
+    parser.add_argument(
+        '-t', '--tile_size',
+        type=int,
+        nargs=2,
+        help='tile size (same for width and height)'
+    )
+    parser.add_argument(
+        '-d', '--dataset',
+        type=Path,
+        help='path to json DICOM dataset'
+    )
+    parser.add_argument(
+        '-l', '--levels',
+        type=int,
+        nargs='+',
+        help='levels to include, if not all',
+    )
+    parser.add_argument(
+        '--no_label',
+        action="store_true",
+        help='if not to include label'
+    )
+    parser.add_argument(
+        '--no_overview',
+        action="store_true",
+        help='if not to include overview'
+    )
+    args = parser.parse_args()
+    if not args.dataset:
+        dataset = create_test_base_dataset()
+    else:
+        json_file = open(args.dataset)
+        dataset = Dataset.from_json(json.load(json_file))
+    if not args.levels:
+        levels = None
+    else:
+        levels = args.levels
+
+    WsiDicomizer.convert(
+        str(args.input),
+        str(args.output),
+        dataset,
+        tile_size=args.tile_size,
+        include_levels=levels,
+        include_label=not args.no_label,
+        include_overview=not args.no_overview
+    )
