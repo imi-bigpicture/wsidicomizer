1701dc7000720bb7ff33b973fba362292e5f65ef
Erik O Gabrielsson <erik.o.gabrilesson@sectra.com>
2021-11-30 16:29:12 +0100

    Add dataset modules to __init__

--- wsidicomizer/__init__.py
+++ wsidicomizer/__init__.py
@@ -12,4 +12,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from .interface import WsiDicomizer
+from wsidicomizer.dataset import (create_brightfield_optical_path_module,
+                                  create_device_module, create_patient_module,
+                                  create_sample, create_specimen_module,
+                                  create_study_module)
+from wsidicomizer.interface import WsiDicomizer
