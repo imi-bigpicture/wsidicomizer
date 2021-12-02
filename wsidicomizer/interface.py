6b33e6934ea87882a1b85166947bc00a70c33e96
Erik O Gabrielsson <erik.o.gabrilesson@sectra.com>
2021-12-02 08:45:44 +0100

    Add open() to WsiDicomizer. Update docstrings

--- wsidicomizer/interface.py
+++ wsidicomizer/interface.py
@@ -2,29 +2,100 @@ import os
 from pathlib import Path
 from typing import Callable, List, Optional, Sequence, Type, Union
 
-from pydicom import config
 from pydicom.dataset import Dataset
 from pydicom.uid import UID as Uid
 from pydicom.uid import generate_uid
+from wsidicom import WsiDicom
 
 from wsidicomizer.common import MetaDicomizer
 from wsidicomizer.czi import CziDicomizer
 from wsidicomizer.openslide import OpenSlideDicomizer
 from wsidicomizer.opentile import OpenTileDicomizer
 
-config.enforce_valid_values = True
-config.future_behavior()
+# List of supported Dicomizers in prioritization order.
+SUPPORTED_TILE_SOURCES: List[Type[MetaDicomizer]] = [
+    OpenTileDicomizer,
+    CziDicomizer,
+    OpenSlideDicomizer
+]
 
 
 class WsiDicomizer:
-    """WsiDicom class with import file-functionality."""
+    """Interface for Dicomizing files."""
+    @staticmethod
+    def open(
+        filepath: str,
+        modules: Optional[Union[Dataset, Sequence[Dataset]]] = None,
+        tile_size: Optional[int] = None,
+        include_levels: Optional[Sequence[int]] = None,
+        include_label: bool = True,
+        include_overview: bool = True,
+        include_confidential: bool = True,
+        encoding_format: str = 'jpeg',
+        encoding_quality: int = 90,
+        jpeg_subsampling: str = '422'
+    ) -> WsiDicom:
+        """Open data in file in filepath as WsiDicom.
+
+        Parameters
+        ----------
+        filepath: str
+            Path to file
+        modules: Optional[Union[Dataset, Sequence[Dataset]]] = None
+            Module datasets to use in files. If none, use default modules.
+        tile_size: int
+            Tile size to use if not defined by file.
+        include_levels: Sequence[int]
+            Optional list of levels to include. Include all levels if None.
+        include_label: bool
+            Include label(s), default true.
+        include_overwiew: bool
+            Include overview(s), default true.
+        include_confidential: bool = True
+            Include confidential metadata.
+        encoding_format: str = 'jpeg'
+            Encoding format to use if re-encoding. 'jpeg' or 'jpeg2000'.
+        encoding_quality: int = 90
+            Quality to use if re-encoding. Do not use > 95 for jpeg. Use 100
+            for lossless jpeg2000.
+        jpeg_subsampling: str = '422'
+            Subsampling option if using jpeg for re-encoding. Use '444' for
+            no subsampling, '422' for 2x2 subsampling.
+
+        Returns
+        ----------
+        WsiDicom
+            WsiDicom object of file.
+        """
+        selected_tile_source = next(
+            (
+                tile_source for tile_source in SUPPORTED_TILE_SOURCES
+                if tile_source.is_supported(filepath)
+            ),
+            None
+        )
+        if selected_tile_source is None:
+            raise NotImplementedError(f"{filepath} is not supported")
+
+        return selected_tile_source.open(
+            filepath,
+            modules,
+            tile_size,
+            include_levels,
+            include_label,
+            include_overview,
+            include_confidential,
+            encoding_format,
+            encoding_quality,
+            jpeg_subsampling
+        )
 
     @classmethod
     def convert(
         cls,
         filepath: str,
         output_path: Optional[str] = None,
-        datasets: Optional[Union[Dataset, Sequence[Dataset]]] = None,
+        modules: Optional[Union[Dataset, Sequence[Dataset]]] = None,
         tile_size: Optional[int] = None,
         uid_generator: Callable[..., Uid] = generate_uid,
         include_levels: Optional[Sequence[int]] = None,
@@ -46,8 +117,8 @@ class WsiDicomizer:
             Path to file
         output_path: str = None
             Folder path to save files to.
-        datasets: Optional[Union[Dataset, Sequence[Dataset]]] = None
-            Base dataset to use in files. If none, use test dataset.
+        modules: Optional[Union[Dataset, Sequence[Dataset]]] = None
+            Module datasets to use in files. If none, use default modules.
         tile_size: int
             Tile size to use if not defined by file.
         uid_generator: Callable[..., Uid] = generate_uid
@@ -79,33 +150,9 @@ class WsiDicomizer:
         List[str]
             List of paths of created files.
         """
-        SUPPORTED_TILE_SOURCES: List[Type[MetaDicomizer]] = [
-            OpenTileDicomizer,
-            CziDicomizer,
-            OpenSlideDicomizer
-        ]
-        selected_tile_source = next(
-            (
-                tile_source for tile_source in SUPPORTED_TILE_SOURCES
-                if tile_source.is_supported(filepath)
-            ),
-            None
-        )
-        if selected_tile_source is None:
-            raise NotImplementedError(f"Not supported format in {filepath}")
-
-        if output_path is None:
-            output_path = str(Path(filepath).parents[0].joinpath(
-                Path(filepath).stem
-            ))
-        try:
-            os.mkdir(output_path)
-        except FileExistsError:
-            ValueError(f'Output path {output_path} already excists')
-
-        with selected_tile_source.open(
+        with cls.open(
             filepath,
-            datasets,
+            modules,
             tile_size,
             include_levels,
             include_label,
@@ -115,6 +162,14 @@ class WsiDicomizer:
             encoding_quality,
             jpeg_subsampling
         ) as wsi:
+            if output_path is None:
+                output_path = str(Path(filepath).parents[0].joinpath(
+                    Path(filepath).stem
+                ))
+            try:
+                os.mkdir(output_path)
+            except FileExistsError:
+                ValueError(f'Output path {output_path} already excists')
             created_files = wsi.save(
                 output_path,
                 uid_generator,
